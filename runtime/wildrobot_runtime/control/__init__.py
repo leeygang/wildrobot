a2ac4757a4b35f97b14d6b5776c8d2ca3aca5f8f
leeygang <leeygang@gmail.com>
2026-01-02 21:21:36 -0800

    remove wildrobot_runtime folder

--- runtime/wildrobot_runtime/control/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Compatibility shim for control entrypoints."""
-
-from __future__ import annotations
--- runtime/wildrobot_runtime/control/run_policy.py
+++
@@ -1,9 +0,0 @@
-"""Compatibility shim.
-
-Old path: wildrobot_runtime.control.run_policy
-New path: wr_runtime.control.run_policy
-"""
-
-from __future__ import annotations
-
-from wr_runtime.control.run_policy import *  # noqa: F403
